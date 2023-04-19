f954582bd7347e8844bc486d88fde0ce6f4388d5
RedFlash <stoqn.sisimen@gmail.com>
2023-04-20 02:21:57 +0300

    gh-pages NEW

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
 import NewMachine from "./Components/NewMachine";
-import SlotMachine from './Components/SlotMachine'
-import NewSlotMachine from "./Components/NewSlotMachine";
 
 function App() {
   return (
     <div className="App">
       <NewMachine/>
-      {/* <SlotMachine/> */}
-      {/* <NewSlotMachine/> */}
     </div>
   );
 }
