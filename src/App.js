8a2174ff6343d7f37c105bba6cb2fd0c8a1407eb
RedFlash <stoqn.sisimen@gmail.com>
2023-04-19 05:22:19 +0300

    Upload to github

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import NewMachine from "./Components/NewMachine";
+import SlotMachine from './Components/SlotMachine'
+import NewSlotMachine from "./Components/NewSlotMachine";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewMachine/>
+      {/* <SlotMachine/> */}
+      {/* <NewSlotMachine/> */}
     </div>
   );
 }
 
 export default App;
+
---
+++ src/Components/NewMachine.js
@@ -0,0 +1,339 @@
+import React, { useEffect, useState, useRef } from "react";
+import gsap from "gsap";
+import lemon from '../Assets/Images/sym_lemon.png'
+import orange from '../Assets/Images/sym_orange.png'
+import plum from '../Assets/Images/sym_plum.png'
+import background from '../Assets/Images/Background.png'
+import frame from '../Assets/Images/frame.png'
+import spritesheet from "../Assets/Images/sym_anim.png";
+import spritesheetData from "../Assets/Images/sym_anim.json";
+import Button from '@mui/material/Button';
+import Alert from "@mui/material/Alert";
+import SpinPay from '../Assets/Sounds/SpinPay.wav'
+import Win from '../Assets/Sounds/Win.wav'
+import BackgroundMelody from '../Assets/Sounds/BackgroundMelody.wav'
+
+    function getRandomElement() {
+    const items = [   
+    <img src={lemon} alt="lemon" id='1'/>,
+    <img src={orange} alt="orange" id='2'/>,
+    <img src={plum} alt="plum" id='3' />,
+    ];
+    const randomElement = items[Math.floor(Math.random() * items.length)];
+    // const randomElement = items[2]
+    return randomElement;
+    }
+
+    function generateNewReels() {
+        const newReels = [];
+        for (let i = 0; i < 3; i++) {
+        const reel1 = getRandomElement();
+        const reel2 = getRandomElement();
+        const reel3 = getRandomElement();
+        newReels.push({ reel1, reel2, reel3});
+        }
+        return newReels;
+    }
+    
+    function NewMachine() {
+    const [reels, setReels] = useState([]);
+    const [isSpinning, setIsSpinning] = useState(false)
+    const [message, setMessage] = useState('')
+    const [startingMessage, setStartingMessage] = useState(true)
+    const [isWin, setIsWin] = useState(false)
+    const buttonSpaceBar = useRef();
+    const BackgroundMusic = new Audio(BackgroundMelody)
+    BackgroundMusic.volume = 0.1;
+    BackgroundMusic.loop = true;
+    BackgroundMusic.play()
+    const handleSpacebar = (event) => {
+        if (event.code === 'Space') {
+          event.preventDefault(); // prevent scrolling
+          buttonSpaceBar.current.click(); // simulate a click on the button
+        }
+      };
+    
+      useEffect(() => {
+        window.addEventListener('keydown', handleSpacebar);
+        return () => window.removeEventListener('keydown', handleSpacebar);
+      }, []);
+
+    useEffect(() => {
+    setReels(generateNewReels())
+    }, []);
+
+    function checkForWin() {
+        const reel1 = document.querySelectorAll(".reel1");
+        const reel2 = document.querySelectorAll(".reel2");
+        const reel3 = document.querySelectorAll(".reel3");
+        let message = "Try again!";
+        let winningCombinations = []
+        const winSound = new Audio(Win)
+        const addWinningElement = (element, reelColumn, reelRow) => {
+            if (element !== false && element !== null){
+                const alt = element.querySelector('img')?.getAttribute('alt')
+                if(alt){
+                    winningCombinations.push({
+                        winningElement: alt,
+                        reelColumn: reelColumn,
+                        reelRow: reelRow,
+                    })
+                }
+            }
+        }
+        if (
+          reel1[0].innerHTML === reel2[0].innerHTML &&
+          reel2[0].innerHTML === reel3[0].innerHTML
+        ) {
+          addWinningElement(reel1[0], 1, 1)
+          message = "You win!";
+          winSound.play()
+        } if (
+          reel1[1].innerHTML === reel2[1].innerHTML &&
+          reel2[1].innerHTML === reel3[1].innerHTML
+        ) {
+            addWinningElement(reel1[1], 2, 2)
+          message = "You win!";
+          winSound.play()
+        } if (
+          reel1[2].innerHTML === reel2[2].innerHTML &&
+          reel2[2].innerHTML === reel3[2].innerHTML
+        ) {
+            addWinningElement(reel1[2], 3, 3)
+          message = "You win!";
+          winSound.play()
+        }
+        setMessage(message);
+        if( winningCombinations.length > 0){
+            setIsWin(true);
+        }
+        return winningCombinations;
+      }
+
+      function stopSpin() {
+        const reel1 = document.querySelectorAll(".reel1");
+        const reel2 = document.querySelectorAll(".reel2");
+        const reel3 = document.querySelectorAll(".reel3");
+        gsap.killTweensOf([reel1, reel2, reel3]);
+        gsap.set([reel1, reel2, reel3], { y: 0 });
+        setIsSpinning(false);
+        checkForWin()
+        winningAnimation(checkForWin())  
+        document.querySelectorAll("div[style*='fixed']").forEach((sprite) => {
+            sprite.remove();
+          })
+    }
+
+    function HandleSpinClick(){
+        if (!isSpinning){
+            Spin()
+            setMessage('Good Luck!')
+        }else{
+            stopSpin()
+    }
+    }
+
+    function winningAnimation(winningCombinations) {
+        const image = new Image();
+        image.src = spritesheet;
+        image.onload = () => {
+          const frames = spritesheetData.frames;
+          const imageNames = {
+            lemon: [
+                "sym_lemon_002","sym_lemon_003","sym_lemon_004","sym_lemon_005","sym_lemon_006","sym_lemon_007","sym_lemon_008","sym_lemon_009","sym_lemon_010","sym_lemon_011","sym_lemon_012","sym_lemon_013",
+                ],
+            orange: [
+                "sym_orange_002","sym_orange_003","sym_orange_004","sym_orange_005","sym_orange_006","sym_orange_007","sym_orange_008","sym_orange_009","sym_orange_010","sym_orange_011","sym_orange_012","sym_orange_013",
+                ],
+            plum: [
+                "sym_plum_002","sym_plum_003","sym_plum_004","sym_plum_005","sym_plum_006","sym_plum_007","sym_plum_008","sym_plum_009","sym_plum_010","sym_plum_011","sym_plum_012","sym_plum_013",
+                ],
+          };
+          const spriteAnimation = gsap.timeline({ repeat: -1 });
+          const timeline = gsap.timeline({ repeat: -1 });
+          winningCombinations.forEach((combo) => {
+            let spritesInCombo = [];
+            const reel = document.querySelectorAll(`.reel1`)[0];
+            const { top, height } = reel.children[0].getBoundingClientRect();
+            const { left, width } = reel.children[0].getBoundingClientRect();
+            const elementName = combo.winningElement;
+
+            if (!imageNames[elementName]) {
+              return;
+            }
+
+            imageNames[elementName].forEach((imageName, index) => {
+              const frame = frames[imageName];
+              const spriteWidth = frame.frame.w;
+              const spriteHeight = frame.frame.h;
+
+              for (let i = combo.reelRow - 1; i < combo.reelRow + 2; i++) {
+                const x = frame.frame.x;
+                const y = frame.frame.y;
+                const sprite = document.createElement("div");
+                sprite.style.position = "fixed";
+                sprite.style.width = spriteWidth + "px";
+                sprite.style.height = spriteHeight + "px";
+                sprite.style.backgroundImage = `url(${spritesheet})`;
+                sprite.style.backgroundPosition = `-${x}px -${y}px`;
+                sprite.style.transform = "scale(0.75)";
+                sprite.style.opacity = 0;
+                document.body.appendChild(sprite);
+                spritesInCombo.push(sprite);
+
+                timeline.to(sprite, {
+                  top: top + (combo.reelRow) * height - 152, // position the sprite based on the row
+                  left: left + (i - combo.reelRow) * width * 1.15 + 115 + i, // move the sprite to the right
+                  duration: 0,
+                });
+              }
+        });
+    const tween = gsap.to(spritesInCombo, {
+        opacity: 1,
+        duration: 0.07,
+        stagger: 0.018,
+        repeat: -1,
+        yoyo: true,
+      });
+      spriteAnimation.add(timeline, 0)
+      spriteAnimation.to({}, { duration: 0.68, onComplete: () => tween.reverse() });
+      spriteAnimation.to({}, { duration: 0.75, onComplete: () => tween.kill() });
+      spriteAnimation.to(spritesInCombo, {
+        opacity: 0,
+        duration: 0.4,
+      });
+    });
+        };
+      }
+      
+    function Spin() {
+            document.querySelectorAll("div[style*='fixed']").forEach((sprite) => {
+            sprite.remove();
+          })
+        const spinPay = new Audio(SpinPay)
+        spinPay.volume = 0.7
+        spinPay.play();
+        const reel1 = document.querySelectorAll(".reel1");
+        const reel2 = document.querySelectorAll(".reel2");
+        const reel3 = document.querySelectorAll(".reel3");
+        const lapCount = Math.floor(Math.random() * 2) + 1;
+        let numberOfSpins = 0
+        setIsSpinning(true)
+        setStartingMessage(false)
+
+        const updateReelElements = (reel, currentLap) => {
+            if (reel === reel1 && currentLap <= lapCount) {
+            reel.forEach((el) => {
+                const newElement = getRandomElement();
+                el.innerHTML = `<img src="${newElement.props.src}" alt="${newElement.props.alt}" />`;
+            });
+            } else if (reel === reel2 && currentLap <= lapCount) {
+            reel.forEach((el) => {
+                const newElement = getRandomElement();
+                el.innerHTML = `<img src="${newElement.props.src}" alt="${newElement.props.alt}" />`;
+            });
+            } else if (reel === reel3 && currentLap <= lapCount) {
+            reel.forEach((el) => {
+                const newElement = getRandomElement();
+                el.innerHTML = `<img src="${newElement.props.src}" alt="${newElement.props.alt}" />`;
+            });
+            }
+        };
+
+        const spinReel = (reel, onComplete) => {
+        let currentLap = 0;
+        let ticker = null;
+            ticker = gsap.ticker.add(() => {
+            currentLap++;
+            updateReelElements(reel, currentLap);
+            if (currentLap >= lapCount || !isSpinning) {
+            gsap.ticker.remove(ticker);
+            onComplete();
+            }
+        });
+
+        gsap.to(reel, {
+            duration: 0.07,
+            y: "+=240",
+            ease: "none",
+            delay: 0,
+            onComplete: () => {
+            gsap.set(reel, { y: "-240" });
+            
+            gsap.to(reel, {
+                duration: 0.25,
+                y: "+=500",
+                ease: "none",
+                delay: 0,
+                onComplete: () => {
+                gsap.set(reel, { y: "-240px" });
+                
+                gsap.to(reel, {
+                    duration: 0.07,
+                    y: "+=240",
+                    ease: "none",
+                    delay: 0,
+                    onComplete: () => {
+                    onComplete();
+                    },
+                });
+                },
+            });
+            },
+        });
+        };
+        spinReel(reel1, () => {
+        numberOfSpins++;
+        spinReel(reel2, () => {
+            numberOfSpins++;
+            spinReel(reel3, () => {
+                numberOfSpins++;
+                if (numberOfSpins === 14){
+                    //fixing bug with the numberOfSpins
+                    setIsSpinning(false)
+                    gsap.killTweensOf([reel1, reel2, reel3]);
+                    gsap.set([reel1, reel2, reel3], {y: 0})
+                    checkForWin()
+                    winningAnimation(checkForWin())
+                }
+            });
+        });
+        });
+    }
+    return (
+        <main>
+        <div class="flex-col h-screen justify-center m-auto content-center bg-center bg-no-repeat" style={{ backgroundImage: `url(${background})`,  backgroundSize:'cover'}}>
+            <div className='Message text-center pt-24'></div>
+            <Alert variant="filled" severity={startingMessage ? 'info' : 'success'} className='w-96 text-lg content-center m-auto mb-5'>
+            {startingMessage ? 'Click the PLAY button to start or spacebar!' : message}
+            </Alert>
+            <div class="overflow-hidden">
+            {/* <audio id="spin-pay" src="../Assets/Sounds/SpinPay.wav"></audio> */}
+            <table class='relative mx-auto' style={{backgroundImage:`url(${frame})`, backgroundSize:'cover'}}>
+                <tbody>
+                {reels.map((e, i) => (
+                    <tr key={i}>
+                    <td className="reel1 lg:w-32 w-28" id='reel-1'>
+                        {e.reel1}
+                        </td>
+                    <td className="reel2 lg:w-36 w-32 pl-4" id='reel-2'>
+                        {e.reel2}
+                    </td>
+                    <td className="reel3 lg:w-36 w-32 pl-4" id='reel-3'>
+                        {e.reel3}
+                    </td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
+            <div class='content-center text-center mt-5'>
+            <Button ref={buttonSpaceBar} variant="contained" size='large' onClick={HandleSpinClick} color={isSpinning ? 'error' : 'success'}>
+            {isSpinning ? "STOP" : "PLAY"}
+            </Button>
+            </div>
+        </div>
+        </main>
+    );
+    }
+    export default NewMachine;
